2aa25066bb44233d4f105aed3decb99a4710bb52
Olaf <olaf.chuszno@gmail.com>
2024-07-21 17:58:53 +0200

    Add additional settings

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,66 @@ function App() {
   const [totalAmount, setTotalAmount] = useState(500000);
   const [installmentType, setInstallmentType] =
     useState<InstallmentType>("fixed");
-  // const [totalInterest, setTotalInterest] = useState<number>();
+
+  const [displayedTotalAmount, setDisplayedTotalAmount] = useState(500000);
+  const [displayedInterest, setDisplayedInterest] = useState(500000);
+  const [displayAdditionalInfo, setDisplayAdditionalInfo] = useState(false);
+
+  // Additional info
+  const [capitalPaidAfterMonths, setCapitalPaidAfterMonths] =
+    useState<number>(0);
+  const [displayedCapitalPaidAfterMonths, setDisplayedCapitalPaidAfterMonths] =
+    useState<number>(0);
+
+  useEffect(() => {
+    // ["1", "5", "5"]
+    const stringAmount = totalAmount.toString().split("");
+
+    // && stringAmount[1] !== ","
+    if (
+      stringAmount.length > 1 &&
+      stringAmount[1] !== "," &&
+      stringAmount[1] !== "."
+    ) {
+      if (stringAmount[0] === "0") {
+        stringAmount.splice(0, 1);
+      }
+    }
+
+    setDisplayedTotalAmount(+stringAmount.join(""));
+  }, [totalAmount]);
+
+  useEffect(() => {
+    // ["1", "5", "5"]
+    const stringMonths = capitalPaidAfterMonths.toString().split("");
+
+    // && stringAmount[1] !== ","
+    if (stringMonths.length > 1) {
+      if (stringMonths[0] === "0") {
+        stringMonths.splice(0, 1);
+      }
+    }
+
+    setDisplayedCapitalPaidAfterMonths(+stringMonths.join(""));
+  }, [capitalPaidAfterMonths]);
+
+  useEffect(() => {
+    // ["1", "5", "5"]
+    const stringInterest = interest.toString().split("");
+
+    // && stringAmount[1] !== ","
+    if (
+      stringInterest.length > 1 &&
+      stringInterest[1] !== "," &&
+      stringInterest[1] !== "."
+    ) {
+      if (stringInterest[0] === "0") {
+        stringInterest.splice(0, 1);
+      }
+    }
+
+    setDisplayedInterest(+stringInterest.join(""));
+  }, [interest]);
 
   const monthlyInterest = useMemo(() => interest / 12, [interest]);
 
@@ -65,6 +124,12 @@ function App() {
           monthlyInterest
         );
 
+  const capitalPaid = useMemo(() => {
+    return data
+      .slice(0, capitalPaidAfterMonths + 1)
+      .reduce((accu, currentMonth) => currentMonth[4] + accu, 0);
+  }, [data, capitalPaidAfterMonths]);
+
   const totalInterest = useMemo(
     () =>
       Math.round(
@@ -73,7 +138,7 @@ function App() {
           0
         ) * 100
       ) / 100,
-    []
+    [data]
   );
 
   return (
@@ -82,9 +147,11 @@ function App() {
         <h1>Kalkulator hipoteczny</h1>
       </header>
 
-      <div className="inputs">
+      <div id="top" className="inputs">
         <div style={{ marginBottom: "50px" }}>
-          <label htmlFor="totalAmount">Rata stała/malejąca</label>
+          <label className="input-label" htmlFor="totalAmount">
+            Rata stała/malejąca
+          </label>
           <select
             id="totalAmount"
             onChange={(event) => {
@@ -100,38 +167,49 @@ function App() {
         </div>
 
         <div style={{ marginBottom: "50px" }}>
-          <label htmlFor="totalAmount">Kwota kredytu (zł)</label>
+          <label className="input-label" htmlFor="totalAmount">
+            Kwota kredytu (zł)
+          </label>
+
           <input
             id="totalAmount"
             onChange={(event) => {
               event.preventDefault();
               setTotalAmount(+event.target.value);
             }}
-            value={totalAmount}
+            value={displayedTotalAmount}
             name="interest"
             type="number"
           />
-          <p>Kwota kredytu (w zł): {totalAmount}</p>
+          <p>Kwota kredytu: {numberWithCommas(totalAmount)} zł</p>
         </div>
+
         <div style={{ marginBottom: "50px" }}>
-          <label htmlFor="interest">Oprocentowanie nominalne (w %)</label>
+          <label className="input-label" htmlFor="interest">
+            Oprocentowanie nominalne (w %)
+          </label>
           <input
             id="interest"
             onChange={(event) => {
               event.preventDefault();
               setInterest(+event.target.value / 100);
             }}
-            value={interest * 100}
+            value={displayedInterest * 100}
             name="interest"
             type="number"
           />
+          <p>
+            Oprocentowanie nominalne (roczne, dziesiętnie): {displayedInterest}
+          </p>
           <p>
             Oprocentowanie miesięczne (zaokrąglone w %):{" "}
             {Math.round(monthlyInterest * 10000) / 100}
           </p>
         </div>
         <div style={{ marginBottom: "50px" }}>
-          <label htmlFor="totalInstallments">Ilość rat (łącznie)</label>
+          <label className="input-label" htmlFor="totalInstallments">
+            Ilość rat (łącznie)
+          </label>
           <input
             id="totalInstallments"
             onChange={(event) => {
@@ -142,27 +220,63 @@ function App() {
             name="totalInstallments"
             type="number"
           />
+
           <p>Łączna ilość rat (w kredycie): {totalInstallments}</p>
         </div>
       </div>
 
-      {installmentType === "fixed" && (
-        <h2>Rata kredytu (zaokrąglona): {roundedInstallment} zł</h2>
+      <div className="display-additional-info">
+        <button onClick={() => setDisplayAdditionalInfo((current) => !current)}>
+          Pokaż Zaawansowane
+        </button>
+      </div>
+
+      {displayAdditionalInfo && (
+        <div className="additional-info">
+          <div className="capital-paid">
+            <label className="input-label" htmlFor="totalInstallments">
+              Kapitał spłacony po x ratach
+            </label>
+
+            <input
+              id="capitalPaid"
+              onChange={(event) => {
+                event.preventDefault();
+                setCapitalPaidAfterMonths(+event.target.value);
+              }}
+              value={displayedCapitalPaidAfterMonths}
+              name="totalInstallments"
+              type="number"
+            />
+
+            <p>
+              Kapitał spłacony po {displayedCapitalPaidAfterMonths}{" "}
+              {capitalPaidAfterMonths === 1 ? "racie" : "ratach"}:{" "}
+              {Math.round(capitalPaid * 100) / 100} zł
+            </p>
+          </div>
+        </div>
       )}
 
-      {installmentType === "decreasing" && (
+      <div className="main-info">
+        {installmentType === "fixed" && (
+          <h2>Rata kredytu: {roundedInstallment} zł</h2>
+        )}
+        {installmentType === "decreasing" && (
+          <h2>
+            Część kapiatłu w racie:{" "}
+            {numberWithCommas(roundedCapitalInEachInstallment)} zł
+          </h2>
+        )}
+
         <h2>
-          Część kapiatłu w racie: {numberWithCommas(roundedInstallment)} zł
+          Łączna suma zapłaconych odsetek: {numberWithCommas(totalInterest)} zł
         </h2>
-      )}
 
-      <h2>
-        Łączna suma zapłaconych odsetek: {numberWithCommas(totalInterest)} zł
-      </h2>
-
-      <a className="bottom-link" href="#end">
-        Przejdź na sam dół
-      </a>
+        <a className="bottom-link" href="#bottom">
+          Przejdź na sam dół
+        </a>
+      </div>
 
       <div className="installments">
         <InstallmentsTable
@@ -172,16 +286,9 @@ function App() {
         />
       </div>
 
-      <h3
-        style={{
-          marginTop: "50px",
-          textAlign: "center",
-          paddingBottom: "100px",
-        }}
-        id="end"
-      >
-        Koniec
-      </h3>
+      <a id="bottom" className="bottom-link" href="#top">
+        Do góry
+      </a>
     </div>
   );
 }
